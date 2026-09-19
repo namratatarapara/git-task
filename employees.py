e2e93c5e9b7ac458828c6f8731405adc1efd97ad
Namrata <namratatarapara@gmail.com>
2026-09-19 17:18:42 +0530

    Intial task

---
+++ employees.py
@@ -0,0 +1,9 @@
+employees = [
+    {"id": 1, "name": "Rahul", "department": "IT", "salary": 35000},
+    {"id": 2, "name": "Priya", "department": "HR", "salary": 32000},
+    {"id": 3, "name": "Amit", "department": "Sales", "salary": 30000},
+    {"id": 4, "name": "Neha", "department": "IT", "salary": 40000},
+]
+
+for employee in employees:
+    print(employee)
